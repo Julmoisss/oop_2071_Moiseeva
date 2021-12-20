8c0d8d6c178d8207118fb61207daeba27fd8f5bb
Julia Moiseeva <moiseeva.yulia2018@yandex.ru>
2021-12-21 00:30:09 +0300

    создание graphics через консоль

---
+++ oop_zadanie_2/oop_zadanie_2/Figure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace oop_zadanie_2
+{
+    abstract class Figure
+    {
+        protected int x, y;
+
+        public abstract void Draw(Graphics gr);
+    }
+}
---
+++ oop_zadanie_2/oop_zadanie_2/Van.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace oop_zadanie_2
+{
+    class Van : Figure
+    {
+        Circle c, c2;
+        Rectangles rect; 
+
+        public Van(int height, int width, int x, int y, Pen pen, Pen penR)
+        {
+
+            rect = new Rectangles(height, width, x, y, pen);
+            c = new Circle(penR, Convert.ToInt32(0.2 * width) + x, (y + height), Convert.ToInt32(0.1 * width));
+            c2 = new Circle(penR, (x + Convert.ToInt32(width*0.7)), (y + height), Convert.ToInt32(0.1 * width));
+        }
+        public override void Draw(Graphics gr)
+        {
+            rect.Draw(gr);
+            c.Draw(gr);
+            c2.Draw(gr);
+        }
+    }
+}
